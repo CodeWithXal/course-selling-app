c35f699595c42fae8d97746b49320638b600a27f
Xalvion <codewithxal@gmail.com>
2025-10-05 15:58:28 +0530

    routes added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {userRouter} = require("./routes/users");
 const {courseRouter} = require("./routes/courses");
+const {adminRouter} = require("./routes/admin");
 
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
+app.use("/admin", adminRouter);
 
 
 
