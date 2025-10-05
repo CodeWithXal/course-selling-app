cd7d8a02d6d97d3f20bb5db22c65abcaa0354dc9
Xalvion <codewithxal@gmail.com>
2025-10-05 15:57:18 +0530

    routes added

---
+++ routes/admin.js
@@ -0,0 +1,45 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+
+async function adminSignup(req, res){
+    res.json({
+        message : "admin signup endpoint"
+    });
+}
+
+async function adminSignin(req, res){
+    res.json({
+        message : "admin signin endpoint"
+    });
+}
+
+async function adminCreateCourse(req, res){
+    res.json({
+        message : "admin create course endpoint"
+    });
+}
+
+async function adminUpdateCourse(req, res){
+    res.json({
+        message : "admin change/update course endpoint"
+    });
+}
+
+async function adminAllCourses(req, res){
+    res.json({
+        message : "admin get all courses endpoint"
+    });
+}
+
+
+
+adminRouter.post("/signup", adminSignup);
+adminRouter.post("/signin", adminSignin);
+adminRouter.post("/course/Create", adminCreateCourse);
+adminRouter.post("/course/update", adminUpdateCourse);
+adminRouter.post("/course/all", adminAllCourses);
+
+module.exports = {
+    adminRouter : adminRouter
+}
