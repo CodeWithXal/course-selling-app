a6ce604183b7e1b864fb484737c9be407477d725
Xalvion <codewithxal@gmail.com>
2025-10-10 20:39:54 +0530

    authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,29 +1,28 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-function authMiddleware(secret_key){
-return function(req, res, next){
+function authMiddleware(secret_key) {
+  return function (req, res, next) {
     const token = req.headers.authorization;
 
-    if(!token){
-        return res.status(401).json({
-            message : "No Token provided"
-        });
+    if (!token) {
+      return res.status(401).json({
+        message: "No Token provided",
+      });
     }
 
-
-    try{
-        const decoded = jwt.verify(token, secret_key);
-        req.userId = decoded.id;
-        next();
+    try {
+      const decoded = jwt.verify(token, secret_key);
+      req.userId = decoded.id;
+      next();
+    } catch (err) {
+      return res.status(401).json({
+        message: "Invalid Token",
+      });
     }
-    catch(err){
-        return res.status(401).json({
-            message : "Invalid Token"
-        });
-    }
-}
+  };
 }
 
 module.exports = {
-    authMiddleware: authMiddleware}
+  authMiddleware: authMiddleware,
+};
