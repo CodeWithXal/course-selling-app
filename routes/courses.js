9d78a385020bf891615a47864f14b54ec0157c4c
Xalvion <codewithxal@gmail.com>
2025-10-30 13:30:21 +0530

    updated

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,36 @@
 const {Router} = require("express");
-
+const {authMiddleware} = require("../middleware/authentication")
 const courseRouter = Router();
-const {courseModel} = require("../db")
+const {courseModel, purchaseModel} = require("../db")
+
+async function course_purchase(req, res){
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+
+    //should do all the price checking, check if the user has already paid the amount
+
+    await purchaseModel.create({
+        userId,
+        courseId    
+
+    })
+
+    res.json({
+        message : "you have successfully purchased the course"
+    });
+}
 
 async function coursePreview(req, res ){
+
+    const courses = await courseModel.find({});
+
     res.json({
-        message : "here are your courses"
+        message : "here are your courses",
+        courses
     });
 }
 courseRouter.get("/preview", coursePreview);
+courseRouter.post("/purchase", authMiddleware, course_purchase)
 
 
 module.exports = {
